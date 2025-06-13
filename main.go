016156a2dd83d2c16655cd2f441d3e823c3842a6
Shinjan saha <shinjansaha02@gmail.com>
2025-06-13 10:02:31 +0530

    Adding prometheus

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
-
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") 
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
@@ -44,6 +44,11 @@ func main() {
 	
 	r.Use(CORSMiddleware())
 
+	
+	p := ginprometheus.NewPrometheus("gin") 
+	p.Use(r)                                
+
+	
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, server is running")
 	})
